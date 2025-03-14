d9335da3898922b67e4201b665072d1da7c2aba6
Leidy <leidijohanaleiva1188@gmail.com>
2025-03-13 22:31:53 -0500

    :sparkles: develop: Hotfix conflicts in header

--- FRONTEND/UNIDADEMPRENDIMIENTO/src/app/shared/components/molecules/header/header.component.ts
+++ FRONTEND/UNIDADEMPRENDIMIENTO/src/app/shared/components/molecules/header/header.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component , Input} from '@angular/core';
-=======
 import { Component, Input,Output,EventEmitter} from '@angular/core';
->>>>>>> a483e393794e99a925fe7b707ccdcaa9e03383cb
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../../atoms/icon/icon.component';
 import { LabelComponent } from '../../atoms/label/label.component';
@@ -16,9 +12,6 @@ import { ButtonwithiconComponent } from '../buttonwithicon/buttonwithicon.compon
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-<<<<<<< HEAD
-  @Input() title: string = 'Gestionar Emprendimientos'; // Título por defecto
-=======
 @Input() title?:string="";
 @Input() icon?:string;
 @Input() classList:string='';
@@ -32,5 +25,4 @@ export class HeaderComponent {
 handleButtonClick() {
   this.btnClick.emit();  // 🔹 Enviar solo el índice
 }
->>>>>>> a483e393794e99a925fe7b707ccdcaa9e03383cb
 }
