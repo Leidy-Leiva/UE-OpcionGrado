420774d39afe6326a5b24a52f0c728c2ba47a923
Mayerly <130258370+Maye15-alt@users.noreply.github.com>
2025-03-13 22:22:17 -0500

    :sparkles: develop: Hotfix conflicts

--- FRONTEND/UNIDADEMPRENDIMIENTO/src/app/shared/components/molecules/buttonwithicon/buttonwithicon.component.ts
+++ FRONTEND/UNIDADEMPRENDIMIENTO/src/app/shared/components/molecules/buttonwithicon/buttonwithicon.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Component,Input,Output,EventEmitter } from '@angular/core';
-=======
-import { Component,Input,Output,EventEmitter} from '@angular/core';
->>>>>>> a483e393794e99a925fe7b707ccdcaa9e03383cb
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../../atoms/icon/icon.component';
 import { ButtonComponent } from "../../atoms/button/button.component";
@@ -18,14 +14,9 @@ export class ButtonwithiconComponent {
 @Input() title?:string;
 @Input() icon:string='';
 @Input() classList:string='';
-<<<<<<< HEAD
-
-@Output() buttonClick = new EventEmitter<Event>();
-=======
 @Input() typeButton: 'button'|'submit'|'reset'='button';
 @Input() disabled=false;
 @Input() iconColor?: string='';  // Nuevo input para el color del icono
  
->>>>>>> a483e393794e99a925fe7b707ccdcaa9e03383cb
 }
  
