a774af3e0ead7f0029339058ce817bb971181af0
Leidy <leidijohanaleiva1188@gmail.com>
2025-03-13 23:11:35 -0500

    :sparkles: develop: Hotfix conflicts complete

--- FRONTEND/UNIDADEMPRENDIMIENTO/src/app/app.component.ts
+++ FRONTEND/UNIDADEMPRENDIMIENTO/src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { ButtonwithiconComponent } from './shared/components/molecules/buttonwithicon/buttonwithicon.component';
-import { ButtonComponent } from './shared/components/atoms/button/button.component';
-import { LabelComponent } from './shared/components/atoms/label/label.component';
-import { HeaderComponent } from "./shared/components/molecules/header/header.component";
-import { RadioButtonComponent } from './shared/components/atoms/radio-button/radio-button.component';
-import { CheckBoxComponent } from './shared/components/atoms/check-box/check-box.component';
-import { ModelComponent } from './shared/components/atoms/model/model.component';
-import { ComboBoxComponent } from './shared/components/atoms/combo-box/combo-box.component';
-import { BquestionEditComponent } from './Features/Bquestions/bquestion-edit/bquestion-edit.component';
-import { SeekerComponent } from './shared/components/molecules/seeker/seeker.component';
-import { TableComponent } from './shared/components/molecules/table/table.component';
+import { LayoutComponent } from './templates/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,HeaderComponent,RadioButtonComponent,CheckBoxComponent,ComboBoxComponent,BquestionEditComponent,RouterOutlet,SeekerComponent,TableComponent],
+  imports: [CommonModule,LayoutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
